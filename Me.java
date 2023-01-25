0ea8f0074e6c238ac53390181f18e7c860a147a8
Mabelle Attieh <Mabellattieh5@gmail.com>
2023-01-25 23:21:18 +0200

    My year of study added

--- Me.java
+++ Me.java
@@ -2,5 +2,6 @@ public class Me{
 	public static void main(String[] args){
 		System.out.println("Mabelle Attieh");
 		System.out.println("Computer Enginnering");
+		System.out.println("2022");
 	}
 }
